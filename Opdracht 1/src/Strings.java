1a38aca3e19c3a63347279359dc19c1bc52f4a0f
Corinej <corine_keijzer@hotmail.com>
2021-02-09 10:33:54 +0100

    eerste java commit

---
+++ Opdracht 1/src/Strings.java
@@ -0,0 +1,15 @@
+
+public class Strings {
+
+	public static void main(String[] args) {
+		System.out.println("Hoi");
+		String a = new String("po");
+		String b = new String("ging");
+		System.out.println("Dit is een " + a + b);
+		String concat = a.concat(b);
+		System.out.println(concat);
+	}
+
+}
+
+
