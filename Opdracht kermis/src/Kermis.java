ceced266040b2087ae75ae77d1f449cfd00a6b5e
Corinej <corine_keijzer@hotmail.com>
2021-02-19 15:31:30 +0100

    kermis stap 2

--- Opdracht kermis/src/Kermis.java
+++ Opdracht kermis/src/Kermis.java
@@ -11,38 +11,39 @@ Kermis() {
 	attracties[5]= new Ladderklimmen();
 }
 void instappen() {
-//	int Switch = 1;
-//	while(Switch == 1) {
+	int Switch = 1;
+	while(Switch == 1) {
 	Scanner sc = new Scanner(System.in);
 	String nummer = sc.nextLine();
 	int keuze = Integer.valueOf(nummer);
 	int keuze2 = keuze - 1;
-	System.out.println(keuze2);
+	//System.out.println(keuze2);
 	System.out.println("U ben in de attractie " + attracties[keuze2].naam + " gestapt");
 	attracties[keuze2].kassa = attracties[keuze2].kassa + attracties[keuze2].prijs;
 	attracties[keuze2].kaartjes = attracties[keuze2].kaartjes + 1;
 	totaalkassa.omzet = totaalkassa.omzet + attracties[keuze2].kassa;
 	totaalkassa.tickets = totaalkassa.tickets + attracties[keuze2].kaartjes;
-//	System.out.println("Wilt u nog een attractie doen? ja of nee");
-//	Scanner antwoord = new Scanner(System.in);
-//	String Antwoord = antwoord.nextLine();
-//	Switch = 0;
-//	if(Antwoord != "ja") {
-//		Switch = 1;
-//	}else {
-//		Switch = 0;
+	System.out.println("Wilt u nog een attractie doen? ja of nee");
+	Scanner antwoord = new Scanner(System.in);
+	String Antwoord = antwoord.nextLine();
+	Switch = 0;
+	if(Antwoord.equals("ja")) {
+		Switch = 1;
+	}else {
+		Switch = 0;
 	
-// }
-//	}
+ }
+	}
 }
 
 
 void tonen() {
+	System.out.println("Kies 'o' voor omzet of 'k' voor kaartjesaantal");
 	Scanner sc2 = new Scanner(System.in);
 	String letter = sc2.nextLine();
-	if(letter != "o") {
+	if(letter.equals("o")) {
 		System.out.println("Er is " +totaalkassa.omzet +" verdiend op de kermis");
-	}else if(letter != "k") {
+	}else if(letter.equals("k")) {
 		System.out.println("Er zijn " +totaalkassa.tickets + " tickets verkocht");
 	}else {
 		System.out.println("Voer een geldige letter in");
